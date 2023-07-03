a1da7673df2d2eca41d05c1fe9c7505f492c10dd
Andrey Emelianov <621077@mail.ru>
2023-07-03 16:24:12 +0300

    Создал контроллеры

---
+++ src/review/review.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Delete, Param, Post, Get } from '@nestjs/common';
+
+import { ReviewModel } from './review.model/review.model';
+
+@Controller('review')
+export class ReviewController {
+  @Post('create')
+  async create(@Body() dto: Omit<ReviewModel, '_id'>) {}
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {}
+
+  @Get('byProduct/:productId')
+  async getByProduct(@Param('productId') productId: string) {}
+}
