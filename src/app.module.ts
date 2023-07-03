082b1ee1254396a3749b7107915d601e7212efa1
Andrey Emelianov <621077@mail.ru>
2023-07-03 19:03:12 +0300

    сделал конфиг для mongo, поднял докер контейнер и успешно подключился

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,12 +8,16 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
 	imports: [
-		MongooseModule.forRoot('mongodb://localhost/test'),
 		ConfigModule.forRoot(),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig,
+		}),
 		AuthModule,
 		TopPageModule,
 		ProductModule,
--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,17 +1,14 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 import { TopPageModel } from './top-page.model/top-page.model';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 
 @Controller('top-page')
 export class TopPageController {
-	constructor(private readonly configService: ConfigService) {}
+	constructor() {}
 
 	@Post('create')
-	async create(@Body() dto: Omit<TopPageModel, '_id'>) {
-		this.configService.get('TEST');
-	}
+	async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
 
 	@Get(':id')
 	async get(@Param('id') id: string) {}
