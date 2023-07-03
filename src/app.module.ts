348a29baebe3ac2c641bef2c22f872f44dab4d0a
Andrey Emelianov <621077@mail.ru>
2023-07-03 17:47:17 +0300

    подключил @nestjs/config и mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		MongooseModule.forRoot('mongodb://localhost/test'),
+		ConfigModule.forRoot(),
+		AuthModule,
+		TopPageModule,
+		ProductModule,
+		ReviewModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,12 +1,17 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { TopPageModel } from './top-page.model/top-page.model';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 
 @Controller('top-page')
 export class TopPageController {
+	constructor(private readonly configService: ConfigService) {}
+
 	@Post('create')
-	async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
+	async create(@Body() dto: Omit<TopPageModel, '_id'>) {
+		this.configService.get('TEST');
+	}
 
 	@Get(':id')
 	async get(@Param('id') id: string) {}
